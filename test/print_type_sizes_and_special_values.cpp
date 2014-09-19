58d21836e2f60ee5f506079f08da29832601fea5
Erik Hvatum <ice.rikh@gmail.com>
2014-09-19 15:39:15 -0500

    Add printing of special values.

--- test/print_type_sizes_and_special_values.cpp
+++ test/print_type_sizes_and_special_values.cpp
@@ -7,4 +7,6 @@ int main(int, char**)
 {
     cout << "typename: size_in_bytes" << endl;
     cout << "sem_t: " << sizeof(sem_t) << endl;
+    cout << "MACRO or other special name: value" << endl;
+    cout << "SEM_FAILED: " << SEM_FAILED << endl;
 }
