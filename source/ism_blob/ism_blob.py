e2b5d667e5c8dd83e753262f196a56d1ab2aa95a
Erik Hvatum <ice.rikh@gmail.com>
2014-09-22 15:57:19 -0500

    Complete reference counted shared memory implementation.

--- source/ism_blob/ism_blob.py
+++ source/ism_blob/ism_blob.py
@@ -23,18 +23,21 @@
 # Authors: Erik Hvatum
 #
 # NB: OS X did not support sharing mutexes across processes until 10.8, at which point support was added for
-# pthread_rwlock sharing only (at last check, the OS X pthread_rwlockattr_setpshared was out of date, erroneously
-# stating that sharing is not supported).  pthread_mutexes are simpler, having only one flag, and about 2x faster
-# accroding to some simple benchmarking.  In order to accomodate OS X, pthread_rwlocks are used instead, and
-# only the pthread_rwlock write flag is utilized.
+# pthread_rwlock sharing only (at last check, the OS X pthread_rwlockattr_setpshared man page was out of date,
+# falsely stating that sharing is not supported).  pthread_mutexes are simpler, having only one flag, and are
+# about 2x faster accroding to some simple benchmarking.  So, in order to accomodate OS X, pthread_rwlocks are
+# used instead, and only the pthread_rwlock write flag is utilized.
 
+import contextlib
 import ctypes
 import errno
 import numpy
 import sys
+import threading
 
 c_uint16_p = ctypes.POINTER(ctypes.c_uint16)
 c_uint32_p = ctypes.POINTER(ctypes.c_uint32)
+c_uint64_p = ctypes.POINTER(ctypes.c_uint64)
 MAP_FAILED = ctypes.cast(-1, ctypes.c_void_p)
 PTHREAD_PROCESS_SHARED = 1
 
@@ -51,6 +54,7 @@ if sys.platform == 'linux':
     pthread_rwlock_init = librt.pthread_rwlock_init
     pthread_rwlock_unlock = librt.pthread_rwlock_unlock
     pthread_rwlock_wrlock = librt.pthread_rwlock_wrlock
+    pthread_rwlockattr_destroy = librt.pthread_rwlockattr_destroy
     pthread_rwlockattr_init = librt.pthread_rwlockattr_init
     pthread_rwlockattr_setpshared = librt.pthread_rwlockattr_setpshared
     O_RDONLY = 0
@@ -64,7 +68,8 @@ if sys.platform == 'linux':
     # NB: 3rd argument, mode_t, is 4 bytes on linux and 2 bytes on osx (64 bit linux and osx, that is.  32
     # bit?  Refer to: http://dilbert.com/strips/comic/1995-06-24/ ...)
     shm_open.argtypes = [ctypes.c_char_p, ctypes.c_int, ctypes.c_uint32]
-    c_rwlockattr_t = ctypes.c_byte * 8
+    c_pthread_rwlockattr_t = ctypes.c_byte * 8
+    c_pthread_rwlock_t = ctypes.c_byte * 56
 elif sys.platform == 'darwin':
     libc = ctypes.CDLL('libc.dylib')
     shm_open   = libc.shm_open
@@ -77,6 +82,7 @@ elif sys.platform == 'darwin':
     pthread_rwlock_init = libc.pthread_rwlock_init
     pthread_rwlock_unlock = libc.pthread_rwlock_unlock
     pthread_rwlock_wrlock = libc.pthread_rwlock_wrlock
+    pthread_rwlockattr_destroy = libc.pthread_rwlockattr_destroy
     pthread_rwlockattr_init = libc.pthread_rwlockattr_init
     pthread_rwlockattr_setpshared = libc.pthread_rwlockattr_setpshared
     O_RDONLY = 0
@@ -88,57 +94,154 @@ elif sys.platform == 'darwin':
     PROT_WRITE = 2
     MAP_SHARED = 1
     shm_open.argtypes = [ctypes.c_char_p, ctypes.c_int, ctypes.c_uint16]
-    c_rwlockattr_t = ctypes.c_byte * 24
+    c_pthread_rwlockattr_t = ctypes.c_byte * 24
+    c_pthread_rwlock_t = ctypes.c_byte * 200
 else:
     raise NotImplementedError('Platform "{}" is unsupported.  Only linux and darwin are supported.')
 
-c_rwlockattr_t_p = ctypes.POINTER(c_rwlockattr_t)
+def _pthread_errcheck(funcName, result, func, args):
+    if result != 0:
+        raise OSError(result, '{}(..) failed: {}'.format(funcName, errno.errorcode.get(result, 'UNKNOWN ERROR')))
+    return result
+
+def _osfunc_errcheck(funcName, result, func, args):
+    if result < 0:
+        e = ctypes.get_errno()
+        if e == 0:
+            raise RuntimeError(funcName + ' failed, but errno is 0.')
+        raise OSError(e, funcName + ' failed: ' + str(errno.errorcode.get(e, 'UNKNOWN ERROR')))
+    return result
+
+def _mmap_errcheck(result, func, args):
+    if result == 0 or result == MAP_FAILED:
+        e = ctypes.get_errno()
+        if e == 0:
+            raise RuntimeError('mmap failed, but errno is 0.')
+        raise OSError(e, 'mmap failed: ' + errno.errorcode.get(e, 'UNKNOWN ERROR'))
+    return result
+
+c_pthread_rwlockattr_t_p = ctypes.POINTER(c_pthread_rwlockattr_t)
+c_pthread_rwlock_t_p = ctypes.POINTER(c_pthread_rwlock_t)
 shm_unlink.argtypes = [ctypes.c_char_p]
 ftruncate.argtypes = [ctypes.c_int, ctypes.c_int64]
 mmap.argtypes = [ctypes.c_void_p, ctypes.c_size_t, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int64]
 mmap.restype = ctypes.c_void_p
+mmap.errcheck = _mmap_errcheck
 munmap.argtypes = [ctypes.c_void_p, ctypes.c_size_t]
 close.argtypes = [ctypes.c_int]
+pthread_rwlock_destroy.argtypes = [c_pthread_rwlock_t_p]
+pthread_rwlock_init.argtypes = [c_pthread_rwlock_t_p, c_pthread_rwlockattr_t_p]
+pthread_rwlock_unlock.argtypes = [c_pthread_rwlock_t_p]
+pthread_rwlock_wrlock.argtypes = [c_pthread_rwlock_t_p]
+pthread_rwlockattr_destroy.argtypes = [c_pthread_rwlockattr_t_p]
+pthread_rwlockattr_init.argtypes = [c_pthread_rwlockattr_t_p]
+pthread_rwlockattr_setpshared.argtypes = [c_pthread_rwlockattr_t_p, ctypes.c_int]
+for funcName in ('pthread_rwlock_destroy',
+                'pthread_rwlock_init',
+                'pthread_rwlock_unlock',
+                'pthread_rwlock_wrlock',
+                'pthread_rwlockattr_destroy',
+                'pthread_rwlockattr_init',
+                'pthread_rwlockattr_setpshared'):
+    eval(funcName).errcheck = lambda result, func, args, funcName=funcName: _pthread_errcheck(funcName, result, func, args)
+for funcName in ('shm_open',
+                 'shm_unlink',
+                 'ftruncate',
+                 'munmap',
+                 'close'):
+    eval(funcName).errcheck = lambda result, func, args, funcName=funcName: _osfunc_errcheck(funcName, result, func, args)
+del funcName
+
+class _ISMBlobHeader(ctypes.Structure):
+    _fields_ = [('refCountLock', c_pthread_rwlock_t),
+                ('refCount', ctypes.c_uint64),
+                ('userSize', ctypes.c_uint64)]
+
+_ISMBlobHeader_p = ctypes.POINTER(_ISMBlobHeader)
+
+@contextlib.contextmanager
+def _refCountLock(p_header):
+    pthread_rwlock_wrlock(ctypes.byref(p_header[0].refCountLock))
+    yield
+    pthread_rwlock_unlock(ctypes.byref(p_header[0].refCountLock))
 
 class ISMBlob:
-    def __init__(self, name, shape, create=True, own=True):
-        self.own = own
-        self.fd = None
-        self.data = None
-        self.name = name.encode('utf-8') if type(name) is str else name
-        self.byteCount = numpy.array(shape).prod() * 2
-        self.ndarray = None
+    def __init__(self, name, size, create=False, createPermissions=0o600):
+        '''Note: 0o600 represents the unix permission value readable/writeable by owner'''
+        if size <= 0:
+            raise ValueError('size must be a positive integer (or floating point value, which will be rounded down).')
+        self._fd = None
+        self._data = None
+        self._p_header = None
+        # Used to determine if destructor should destroy shared region upon allocation failure
+        self._isCreator = create
+        self._name = str(name).encode('utf-8') if type(name) is not bytes else name
+        self._userSize = size
+        self._size = size + ctypes.sizeof(_ISMBlobHeader)
         if create:
-            # NB: 0o600 represents the unix permission value readable/writeable by owner
-            self.fd = shm_open(self.name, O_RDWR | O_CREAT | O_EXCL, 0o600)
-            if self.fd == -1:
-                self._rose('shm_open')
-            if ftruncate(self.fd, self.byteCount) == -1:
-                self._rose('ftruncate')
+            self._fd = shm_open(self._name, O_RDWR | O_CREAT | O_EXCL, createPermissions)
+            ftruncate(self._fd, self._size)
         else:
-            self.fd = shm_open(self.name, O_RDWR, 0)
-            if self.fd == -1:
-                self._rose('shm_open')
-        data = mmap(ctypes.c_void_p(0), self.byteCount, PROT_READ | PROT_WRITE, MAP_SHARED, self.fd, 0)
-        if data == MAP_FAILED or data == ctypes.c_void_p(0):
-            self._rose('mmap')
-        self.data = ctypes.cast(data, c_uint16_p)
-        self.ndarray = numpy.ctypeslib.as_array(self.data, shape)
+            self._fd = shm_open(self._name, O_RDWR, 0)
+        self._data = mmap(ctypes.c_void_p(0), self._size, PROT_READ | PROT_WRITE, MAP_SHARED, self._fd, 0)
+        self._userData = self._data + ctypes.sizeof(_ISMBlobHeader)
+        self._p_header = ctypes.cast(self._data, _ISMBlobHeader_p)
+        if create:
+            lockAttr = c_pthread_rwlockattr_t()
+            p_lockAttr = ctypes.pointer(lockAttr)
+            pthread_rwlockattr_init(p_lockAttr)
+            pthread_rwlockattr_setpshared(p_lockAttr, PTHREAD_PROCESS_SHARED)
+            pthread_rwlock_init(ctypes.byref(self._p_header[0].refCountLock), p_lockAttr)
+            pthread_rwlockattr_destroy(p_lockAttr)
+            with _refCountLock(self._p_header):
+                self._p_header[0].refCount = 1
+                self._p_header[0].userSize = self._userSize
+        else:
+            with _refCountLock(self._p_header):
+                self._p_header[0].refCount += 1
+            if self._p_header[0].userSize != self._userSize:
+                raise ValueError('Supplied size ({}) does not match shared region size ({}).'.format(self._userSize, self._p_header[0].userSize))
 
     def __del__(self):
-        del self.ndarray
-        if self.data is not None:
-            munmap(ctypes.cast(self.data, ctypes.c_void_p), self.byteCount)
-        if self.fd is not None:
-            if close(self.fd) == -1:
-                self._rose('close')
-            if self.own:
-                if shm_unlink(self.name) == -1:
-                    self._rose('shm_unlink')
+        if self._p_header is not None:
+            destroy = False
+            try:
+                # The refCount lock resides within the shared memory region to be destroyed and thus does not prevent
+                # another thread in the same process from initiating a deep copy of this ISM object in the interval between
+                # refCount lock release and completion of this destructor function.  However, if this destructor is executing,
+                # it is as a consequence of no Python references to this object remaining, precluding the possibility of a copy
+                # operation being initiated except behind the interpreter's back.  Therefore, a second in-process lock
+                # to guard against this is not required.  Additionally, if refCount reaches zero below, no other processes
+                # are still referring to the shared memory, precluding the possibility of a copy operation in *another process*
+                # during the interval between refLock destruction and completion of this destructor function, so a second
+                # shared-process lock to guard against this is not required.
+                #
+                # The purpose of locating this logic within try/finally is to ensure correct behavior even if ctrl-c is received
+                # during destructor execution.
+                with _refCountLock(self._p_header):
+                    self._p_header[0].refCount -= 1
+                    if self._p_header[0].refCount == 0:
+                        destroy = True
+            finally:
+                if destroy:
+                    munmap(self._data, self._size)
+                    close(self._fd)
+                    shm_unlink(self._name)
+                    return
+        elif self._fd is not None:
+            close(self._fd)
+            if self._isCreator:
+                # Oops, allocation failed.  Get rid of the partially initialized shared memory region.
+                shm_unlink(self._name)
 
-    @staticmethod
-    def _rose(fname):
-        e = ctypes.get_errno()
-        if e == 0:
-            raise RuntimeError(fname + ' failed, but errno is 0.')
-        raise OSError(e, errno.errorcode.get(e, 'UNKNOWN ERROR'))
+    @property
+    def name(self):
+        return self._name.decode('utf-8')
+
+    @property
+    def size(self):
+        return self._userSize
+
+    @property
+    def data(self):
+        return self._userData        
