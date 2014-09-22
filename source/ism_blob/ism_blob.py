c31f1d46c2f4f9f62ad016cbb787f462139bcff1
Erik Hvatum <ice.rikh@gmail.com>
2014-09-22 18:24:48 -0500

    Prepend internal implementation detail module variables with _.

--- source/ism_blob/ism_blob.py
+++ source/ism_blob/ism_blob.py
@@ -36,67 +36,62 @@ import numpy
 import sys
 import threading
 
-c_uint16_p = ctypes.POINTER(ctypes.c_uint16)
-c_uint32_p = ctypes.POINTER(ctypes.c_uint32)
-c_uint64_p = ctypes.POINTER(ctypes.c_uint64)
-MAP_FAILED = ctypes.cast(-1, ctypes.c_void_p)
-PTHREAD_PROCESS_SHARED = 1
+_MAP_FAILED = ctypes.cast(-1, ctypes.c_void_p)
+_PTHREAD_PROCESS_SHARED = 1
 
 if sys.platform == 'linux':
-    libc = ctypes.CDLL('libc.so.6')
-    librt = ctypes.CDLL('librt.so.1')
-    shm_open   = librt.shm_open
-    shm_unlink = librt.shm_unlink
-    ftruncate  = libc.ftruncate
-    mmap       = libc.mmap
-    munmap     = libc.munmap
-    close      = libc.close
-    pthread_rwlock_destroy = librt.pthread_rwlock_destroy
-    pthread_rwlock_init = librt.pthread_rwlock_init
-    pthread_rwlock_unlock = librt.pthread_rwlock_unlock
-    pthread_rwlock_wrlock = librt.pthread_rwlock_wrlock
-    pthread_rwlockattr_destroy = librt.pthread_rwlockattr_destroy
-    pthread_rwlockattr_init = librt.pthread_rwlockattr_init
-    pthread_rwlockattr_setpshared = librt.pthread_rwlockattr_setpshared
-    O_RDONLY = 0
-    O_RDWR   = 2
-    O_CREAT  = 64
-    O_EXCL   = 128
-    O_TRUNC  = 512
-    PROT_READ  = 1
-    PROT_WRITE = 2
-    MAP_SHARED = 1
+    _libc = ctypes.CDLL('libc.so.6')
+    _librt = ctypes.CDLL('librt.so.1')
+    _shm_open   = _librt.shm_open
+    _shm_unlink = _librt.shm_unlink
+    _ftruncate  = _libc.ftruncate
+    _mmap       = _libc.mmap
+    _munmap     = _libc.munmap
+    _close      = _libc.close
+    _pthread_rwlock_destroy = _librt.pthread_rwlock_destroy
+    _pthread_rwlock_init = _librt.pthread_rwlock_init
+    _pthread_rwlock_unlock = _librt.pthread_rwlock_unlock
+    _pthread_rwlock_wrlock = _librt.pthread_rwlock_wrlock
+    _pthread_rwlockattr_destroy = _librt.pthread_rwlockattr_destroy
+    _pthread_rwlockattr_init = _librt.pthread_rwlockattr_init
+    _pthread_rwlockattr_setpshared = _librt.pthread_rwlockattr_setpshared
+    _O_RDONLY = 0
+    _O_RDWR   = 2
+    _O_CREAT  = 64
+    _O_EXCL   = 128
+    _PROT_READ  = 1
+    _PROT_WRITE = 2
+    _MAP_SHARED = 1
     # NB: 3rd argument, mode_t, is 4 bytes on linux and 2 bytes on osx (64 bit linux and osx, that is.  32
     # bit?  Refer to: http://dilbert.com/strips/comic/1995-06-24/ ...)
-    shm_open.argtypes = [ctypes.c_char_p, ctypes.c_int, ctypes.c_uint32]
-    c_pthread_rwlockattr_t = ctypes.c_byte * 8
-    c_pthread_rwlock_t = ctypes.c_byte * 56
+    _shm_open.argtypes = [ctypes.c_char_p, ctypes.c_int, ctypes.c_uint32]
+    _c_pthread_rwlockattr_t = ctypes.c_byte * 8
+    _c_pthread_rwlock_t = ctypes.c_byte * 56
 elif sys.platform == 'darwin':
-    libc = ctypes.CDLL('libc.dylib')
-    shm_open   = libc.shm_open
-    shm_unlink = libc.shm_unlink
-    ftruncate  = libc.ftruncate
-    mmap       = libc.mmap
-    munmap     = libc.munmap
-    close      = libc.close
-    pthread_rwlock_destroy = libc.pthread_rwlock_destroy
-    pthread_rwlock_init = libc.pthread_rwlock_init
-    pthread_rwlock_unlock = libc.pthread_rwlock_unlock
-    pthread_rwlock_wrlock = libc.pthread_rwlock_wrlock
-    pthread_rwlockattr_destroy = libc.pthread_rwlockattr_destroy
-    pthread_rwlockattr_init = libc.pthread_rwlockattr_init
-    pthread_rwlockattr_setpshared = libc.pthread_rwlockattr_setpshared
-    O_RDONLY = 0
-    O_RDWR   = 2
-    O_CREAT  = 512
-    O_EXCL   = 2048
-    O_TRUNC  = 1024
-    PROT_READ  = 1
-    PROT_WRITE = 2
-    MAP_SHARED = 1
-    shm_open.argtypes = [ctypes.c_char_p, ctypes.c_int, ctypes.c_uint16]
-    c_pthread_rwlockattr_t = ctypes.c_byte * 24
-    c_pthread_rwlock_t = ctypes.c_byte * 200
+    _libc = ctypes.CDLL('libc.dylib')
+    _shm_open   = _libc.shm_open
+    _shm_unlink = _libc.shm_unlink
+    _ftruncate  = _libc.ftruncate
+    _mmap       = _libc.mmap
+    _munmap     = _libc.munmap
+    _close      = _libc.close
+    _pthread_rwlock_destroy = _libc.pthread_rwlock_destroy
+    _pthread_rwlock_init = _libc.pthread_rwlock_init
+    _pthread_rwlock_unlock = _libc.pthread_rwlock_unlock
+    _pthread_rwlock_wrlock = _libc.pthread_rwlock_wrlock
+    _pthread_rwlockattr_destroy = _libc.pthread_rwlockattr_destroy
+    _pthread_rwlockattr_init = _libc.pthread_rwlockattr_init
+    _pthread_rwlockattr_setpshared = _libc.pthread_rwlockattr_setpshared
+    _O_RDONLY = 0
+    _O_RDWR   = 2
+    _O_CREAT  = 512
+    _O_EXCL   = 2048
+    _PROT_READ  = 1
+    _PROT_WRITE = 2
+    _MAP_SHARED = 1
+    _shm_open.argtypes = [ctypes.c_char_p, ctypes.c_int, ctypes.c_uint16]
+    _c_pthread_rwlockattr_t = ctypes.c_byte * 24
+    _c_pthread_rwlock_t = ctypes.c_byte * 200
 else:
     raise NotImplementedError('Platform "{}" is unsupported.  Only linux and darwin are supported.')
 
@@ -114,47 +109,47 @@ def _osfunc_errcheck(funcName, result, func, args):
     return result
 
 def _mmap_errcheck(result, func, args):
-    if result == 0 or result == MAP_FAILED:
+    if result == 0 or result == _MAP_FAILED:
         e = ctypes.get_errno()
         if e == 0:
             raise RuntimeError('mmap failed, but errno is 0.')
         raise OSError(e, 'mmap failed: ' + errno.errorcode.get(e, 'UNKNOWN ERROR'))
     return result
 
-c_pthread_rwlockattr_t_p = ctypes.POINTER(c_pthread_rwlockattr_t)
-c_pthread_rwlock_t_p = ctypes.POINTER(c_pthread_rwlock_t)
-shm_unlink.argtypes = [ctypes.c_char_p]
-ftruncate.argtypes = [ctypes.c_int, ctypes.c_int64]
-mmap.argtypes = [ctypes.c_void_p, ctypes.c_size_t, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int64]
-mmap.restype = ctypes.c_void_p
-mmap.errcheck = _mmap_errcheck
-munmap.argtypes = [ctypes.c_void_p, ctypes.c_size_t]
-close.argtypes = [ctypes.c_int]
-pthread_rwlock_destroy.argtypes = [c_pthread_rwlock_t_p]
-pthread_rwlock_init.argtypes = [c_pthread_rwlock_t_p, c_pthread_rwlockattr_t_p]
-pthread_rwlock_unlock.argtypes = [c_pthread_rwlock_t_p]
-pthread_rwlock_wrlock.argtypes = [c_pthread_rwlock_t_p]
-pthread_rwlockattr_destroy.argtypes = [c_pthread_rwlockattr_t_p]
-pthread_rwlockattr_init.argtypes = [c_pthread_rwlockattr_t_p]
-pthread_rwlockattr_setpshared.argtypes = [c_pthread_rwlockattr_t_p, ctypes.c_int]
-for funcName in ('pthread_rwlock_destroy',
-                'pthread_rwlock_init',
-                'pthread_rwlock_unlock',
-                'pthread_rwlock_wrlock',
-                'pthread_rwlockattr_destroy',
-                'pthread_rwlockattr_init',
-                'pthread_rwlockattr_setpshared'):
+_c_pthread_rwlockattr_t_p = ctypes.POINTER(_c_pthread_rwlockattr_t)
+_c_pthread_rwlock_t_p = ctypes.POINTER(_c_pthread_rwlock_t)
+_shm_unlink.argtypes = [ctypes.c_char_p]
+_ftruncate.argtypes = [ctypes.c_int, ctypes.c_int64]
+_mmap.argtypes = [ctypes.c_void_p, ctypes.c_size_t, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int64]
+_mmap.restype = ctypes.c_void_p
+_mmap.errcheck = _mmap_errcheck
+_munmap.argtypes = [ctypes.c_void_p, ctypes.c_size_t]
+_close.argtypes = [ctypes.c_int]
+_pthread_rwlock_destroy.argtypes = [_c_pthread_rwlock_t_p]
+_pthread_rwlock_init.argtypes = [_c_pthread_rwlock_t_p, _c_pthread_rwlockattr_t_p]
+_pthread_rwlock_unlock.argtypes = [_c_pthread_rwlock_t_p]
+_pthread_rwlock_wrlock.argtypes = [_c_pthread_rwlock_t_p]
+_pthread_rwlockattr_destroy.argtypes = [_c_pthread_rwlockattr_t_p]
+_pthread_rwlockattr_init.argtypes = [_c_pthread_rwlockattr_t_p]
+_pthread_rwlockattr_setpshared.argtypes = [_c_pthread_rwlockattr_t_p, ctypes.c_int]
+for funcName in ('_pthread_rwlock_destroy',
+                '_pthread_rwlock_init',
+                '_pthread_rwlock_unlock',
+                '_pthread_rwlock_wrlock',
+                '_pthread_rwlockattr_destroy',
+                '_pthread_rwlockattr_init',
+                '_pthread_rwlockattr_setpshared'):
     eval(funcName).errcheck = lambda result, func, args, funcName=funcName: _pthread_errcheck(funcName, result, func, args)
-for funcName in ('shm_open',
-                 'shm_unlink',
-                 'ftruncate',
-                 'munmap',
-                 'close'):
+for funcName in ('_shm_open',
+                 '_shm_unlink',
+                 '_ftruncate',
+                 '_munmap',
+                 '_close'):
     eval(funcName).errcheck = lambda result, func, args, funcName=funcName: _osfunc_errcheck(funcName, result, func, args)
 del funcName
 
 class _ISMBlobHeader(ctypes.Structure):
-    _fields_ = [('refCountLock', c_pthread_rwlock_t),
+    _fields_ = [('refCountLock', _c_pthread_rwlock_t),
                 ('refCount', ctypes.c_uint64),
                 ('userSize', ctypes.c_uint64)]
 
@@ -162,9 +157,9 @@ _ISMBlobHeader_p = ctypes.POINTER(_ISMBlobHeader)
 
 @contextlib.contextmanager
 def _refCountLock(p_header):
-    pthread_rwlock_wrlock(ctypes.byref(p_header[0].refCountLock))
+    _pthread_rwlock_wrlock(ctypes.byref(p_header[0].refCountLock))
     yield
-    pthread_rwlock_unlock(ctypes.byref(p_header[0].refCountLock))
+    _pthread_rwlock_unlock(ctypes.byref(p_header[0].refCountLock))
 
 _dts_to_cts = {}
 for t in (ctypes.c_float, ctypes.c_double):
@@ -221,20 +216,20 @@ class ISMBlob:
         self._userSize = size
         self._size = size + ctypes.sizeof(_ISMBlobHeader)
         if create:
-            self._fd = shm_open(self._name, O_RDWR | O_CREAT | O_EXCL, createPermissions)
-            ftruncate(self._fd, self._size)
+            self._fd = _shm_open(self._name, _O_RDWR | _O_CREAT | _O_EXCL, createPermissions)
+            _ftruncate(self._fd, self._size)
         else:
-            self._fd = shm_open(self._name, O_RDWR, 0)
-        self._data = mmap(ctypes.c_void_p(0), self._size, PROT_READ | PROT_WRITE, MAP_SHARED, self._fd, 0)
+            self._fd = _shm_open(self._name, _O_RDWR, 0)
+        self._data = _mmap(ctypes.c_void_p(0), self._size, _PROT_READ | _PROT_WRITE, _MAP_SHARED, self._fd, 0)
         self._userData = self._data + ctypes.sizeof(_ISMBlobHeader)
         self._p_header = ctypes.cast(self._data, _ISMBlobHeader_p)
         if create:
-            lockAttr = c_pthread_rwlockattr_t()
+            lockAttr = _c_pthread_rwlockattr_t()
             p_lockAttr = ctypes.pointer(lockAttr)
-            pthread_rwlockattr_init(p_lockAttr)
-            pthread_rwlockattr_setpshared(p_lockAttr, PTHREAD_PROCESS_SHARED)
-            pthread_rwlock_init(ctypes.byref(self._p_header[0].refCountLock), p_lockAttr)
-            pthread_rwlockattr_destroy(p_lockAttr)
+            _pthread_rwlockattr_init(p_lockAttr)
+            _pthread_rwlockattr_setpshared(p_lockAttr, _PTHREAD_PROCESS_SHARED)
+            _pthread_rwlock_init(ctypes.byref(self._p_header[0].refCountLock), p_lockAttr)
+            _pthread_rwlockattr_destroy(p_lockAttr)
             with _refCountLock(self._p_header):
                 self._p_header[0].refCount = 1
                 self._p_header[0].userSize = self._userSize
@@ -266,15 +261,15 @@ class ISMBlob:
                         destroy = True
             finally:
                 if destroy:
-                    munmap(self._data, self._size)
-                    close(self._fd)
-                    shm_unlink(self._name)
+                    _munmap(self._data, self._size)
+                    _close(self._fd)
+                    _shm_unlink(self._name)
                     return
         elif self._fd is not None:
-            close(self._fd)
+            _close(self._fd)
             if self._isCreator:
                 # Oops, allocation failed.  Get rid of the partially initialized shared memory region.
-                shm_unlink(self._name)
+                _shm_unlink(self._name)
 
     @classmethod
     def create_with_numpy_view(cls, name, shape, dtype, permissions=0o600):
